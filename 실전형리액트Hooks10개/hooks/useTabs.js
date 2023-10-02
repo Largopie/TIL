b1c5a7436448e045e607a74e8dfac841d70826a4
Largopie <106071687+Largopie@users.noreply.github.com>
2023-10-02 18:08:49 +0900

    hooks 레포지토리 별도 생성으로 삭제

--- 실전형리액트Hooks10개/hooks/useTabs.js
+++
@@ -1,9 +0,0 @@
-export const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if (!allTabs || !Array.isArray(allTabs)) return;
-  
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex
-  };
-};
